84a8b414bf84ca458cce48594036bbac2564560c
Ilgu Lee <iglee2000ca@gmail.com>
2018-06-06 06:37:55 -0400

    Keep state when changing orientation using onSaveInstanceState,onRestoreInstanceState

--- app/src/main/java/ilgulee/com/funfacts/FunFactActivity.java
+++ app/src/main/java/ilgulee/com/funfacts/FunFactActivity.java
@@ -9,9 +9,14 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class FunFactActivity extends AppCompatActivity {
+    private static final String KEY_FACT = "KEY_FACT";
+    private static final String KEY_COLOR = "KEY_COLOR";
     private TextView factTextView;
     private Button showFactButton;
     private RelativeLayout relativeLayout;
+    private String mFact=FactBook.facts[0];
+    private String mColor=FactBook.backgroundColors[8];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,21 +32,35 @@ public class FunFactActivity extends AppCompatActivity {
 //
 //            }
 //        };
-//        showFactButton.setOnClickListener(listener);
-
+//
         showFactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
                 //String fact= String.valueOf(randNum);
+                mFact=FactBook.getRandomFact();
+                factTextView.setText(mFact);
 
-                factTextView.setText(FactBook.getRandomFact());
-
-                String color=FactBook.getBackgroundColor();
-
-                relativeLayout.setBackgroundColor(Color.parseColor(color));
-
-                showFactButton.setTextColor(Color.parseColor(color));
+                mColor=FactBook.getBackgroundColor();
+                relativeLayout.setBackgroundColor(Color.parseColor(mColor));
+                showFactButton.setTextColor(Color.parseColor(mColor));
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(KEY_FACT,mFact);
+        outState.putString(KEY_COLOR,mColor);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mFact=savedInstanceState.getString(KEY_FACT);
+        factTextView.setText(mFact);
+        mColor=savedInstanceState.getString(KEY_COLOR);
+        relativeLayout.setBackgroundColor(Color.parseColor(mColor));
+        showFactButton.setTextColor(Color.parseColor(mColor));
     }
 }
